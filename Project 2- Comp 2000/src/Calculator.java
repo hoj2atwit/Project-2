7d68d2aad4a897d364e09ef8c2062b908b7ef76e
hoj2 <hoj2@WIT45001220.wit.private>
2019-10-04 11:14:51 -0400

    boop

---
+++ Project 2- Comp 2000/src/Calculator.java
@@ -0,0 +1,56 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class Calculator implements Initializable{
+
+	@FXML
+	private Button quitButton;
+	@FXML
+	private Button button1;
+	@FXML
+	private Button button2;
+	@FXML
+	private Button button3;
+	@FXML
+	private Button button4;
+	@FXML
+	private Button button5;
+	@FXML
+	private Button button6;
+	@FXML
+	private Button button7;
+	@FXML
+	private Button button8;
+	@FXML
+	private Button button9;
+	@FXML
+	private Button button0;
+	@FXML
+	private Button subtractButton;
+	@FXML
+	private Button plusButton;
+	@FXML
+	private Button multiplyButton;
+	@FXML
+	private Button divideButton;
+	@FXML
+	private Button clearButton;
+	@FXML
+	private Button deleteButton;
+	@FXML
+	private Button openPButton;
+	@FXML
+	private Button closedPButton;
+	@FXML
+	private Button equalsButton;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
