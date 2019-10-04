d4856288ca323f22f66d4840737e21919906c6be
hoj2 <hoj2@WIT45001220.wit.private>
2019-10-04 15:25:09 -0400

    Added keyboard Functionality

--- Project 2- Comp 2000/src/CalculatorController.java
+++ Project 2- Comp 2000/src/CalculatorController.java
@@ -1,8 +1,16 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
-public class CalculatorController extends Calculator {
+public class CalculatorController extends Calculator implements Initializable {
 	@FXML
 	private static Button quitButton;
 	@FXML
@@ -47,6 +55,8 @@ public class CalculatorController extends Calculator {
 	private Text operationText;
 	@FXML
 	private Text errorText;
+	@FXML
+	private AnchorPane calculatorWindow;
 	
 	public void update() {
 		operationText.setText(getInfix());
@@ -154,9 +164,140 @@ public class CalculatorController extends Calculator {
 		System.exit(0);
 	}
 	
-	public void equals() {
+	public void equal() {
 		calculate();
 		update();
 	}
 
+	/**
+	 * Method that holds what each button does within the world
+	 */
+	public void keyPressedShift() {
+		calculatorWindow.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent e) {
+				if(e.getCode() == KeyCode.DIGIT8) { 
+		    		  multiply();
+		    	}
+				if(e.getCode() == KeyCode.EQUALS) { 
+		    		  add();
+		    	}
+				if(e.getCode() == KeyCode.DIGIT9) { 
+		    		  openParenthesis();
+		    	}
+				if(e.getCode() == KeyCode.DIGIT0) { 
+		    		  closedParenthesis();
+		    	}
+				if(e.getCode() == KeyCode.ENTER) { 
+		    		  equal();
+		    	}
+				if(e.getCode() == KeyCode.ESCAPE || e.getCode() == KeyCode.Q) {
+		    		 quit();
+		    	}
+				if(e.getCode() == KeyCode.C) { 
+		    		  clear();
+				}
+				if(e.getCode() == KeyCode.BACK_SPACE) { 
+		    		  delete();
+		    	  }
+				e.consume();
+			}
+			
+		});
+		calculatorWindow.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent e) {
+				if(e.getCode() == KeyCode.SHIFT) {
+					keyPressedNormal();
+				}
+				e.consume();
+			}
+			
+		});
+		
+			
+	}
+	
+	/**
+	 * Method that holds what each button does within the world
+	 */
+	public void keyPressedNormal() {
+		calculatorWindow.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		      @Override
+		      public void handle(KeyEvent e) {
+		    	  if(e.getCode() == KeyCode.NUMPAD1 || e.getCode() == KeyCode.DIGIT1) {
+		    		  one();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD2 || e.getCode() == KeyCode.DIGIT2) {
+		    		  two();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD3 || e.getCode() == KeyCode.DIGIT3) {
+		    		  three();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD4 || e.getCode() == KeyCode.DIGIT4) { 
+		    		  four();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD5 || e.getCode() == KeyCode.DIGIT5) { 
+		    		  five();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD6 || e.getCode() == KeyCode.DIGIT6) { 
+		    		  six();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD7 || e.getCode() == KeyCode.DIGIT7) { 
+		    		  seven();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD8 || e.getCode() == KeyCode.DIGIT8) { 
+		    		  eight();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD9 || e.getCode() == KeyCode.DIGIT9) { 
+		    		  nine();
+		    	  }
+		    	  if(e.getCode() == KeyCode.NUMPAD0 || e.getCode() == KeyCode.DIGIT0) { 
+		    		  zero();
+		    	  }
+		    	  if(e.getCode() == KeyCode.BACK_SPACE) { 
+		    		  delete();
+		    	  }
+		    	  if(e.getCode() == KeyCode.ASTERISK) { 
+		    		  multiply();
+		    	  }
+		    	  if(e.getCode() == KeyCode.BACK_SLASH) { 
+		    		  divide();
+		    	  }
+		    	  if(e.getCode() == KeyCode.PLUS) { 
+		    		  add();
+		    	  }
+		    	  if(e.getCode() == KeyCode.MINUS) { 
+		    		  subtract();
+		    	  }
+		    	  if(e.getCode() == KeyCode.LEFT_PARENTHESIS) { 
+		    		  openParenthesis();
+		    	  }
+		    	  if(e.getCode() == KeyCode.RIGHT_PARENTHESIS) { 
+		    		  closedParenthesis();
+		    	  }
+		    	  if(e.getCode() == KeyCode.ENTER || e.getCode() == KeyCode.EQUALS) { 
+		    		  equal();
+		    	  }
+		    	  if(e.getCode() == KeyCode.C) { 
+		    		  clear();
+		    	  }
+		    	  if(e.getCode() == KeyCode.SHIFT) {
+		    		  keyPressedShift();
+		    	  }
+		    	  if(e.getCode() == KeyCode.ESCAPE || e.getCode() == KeyCode.Q) {
+		    		 quit();
+		    	  }
+		    	  e.consume();
+		        }
+		    });
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		keyPressedNormal();
+		
+	}
+	
 }
